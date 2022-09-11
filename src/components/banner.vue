512f2fdbabf4c4ecbbd7e9bbd275d90816409f18
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-09-11 23:45:30 +0300

    Banner component has been added & other small changes

---
+++ src/components/banner.vue
@@ -0,0 +1,9 @@
+<template>
+  <a
+    target="_blank"
+    href="https://github.com/ubeydeozdmr/crema-todo"
+    class="rounded text-xs text-center w-full bg-gray-200 dark:bg-gray-700 dark:text-gray-300 fixed bottom-0 left-0"
+  >
+    Created by Ubeyde Emir Özdemir with ❤️ Click for GitHub link
+  </a>
+</template>
